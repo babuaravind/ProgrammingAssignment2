7337c0a9c23c90a03d56dc1885104abb140ec7ae
babuaravind <itsbabuaravind@gmail.com>
2020-03-21 18:01:00 +0530

    Functions work as intended.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## The functions are used to find the inverse of any mxn matrix.
+## The function will cache the matrix and return the result from
+## the cached copy if the user requests it again.
 
 ## Write a short comment describing this function
-
+## this function is used to store the matrix and it's inverse.
+## We can also retrieve the stored matrix by using get and getMatrixInverse inner functions.
 makeCacheMatrix <- function(x = matrix()) {
-
+  cached_i <- NULL
+  set <- function(y) {
+    x <<- y
+    cached_i <<- NULL
+  }
+  get <- function() {
+    return(x)
+  }
+  setMatrixInverse <- function(matrixInverse) {
+    cached_i <<- matrixInverse
+  } 
+  getMatrixInverse <- function() {
+    return(cached_i)
+  }
+  list(set = set,
+       get = get,
+       setMatrixInverse = setMatrixInverse,
+       getMatrixInverse = getMatrixInverse)
 }
 
 
 ## Write a short comment describing this function
+## The function cacheSolve() returns the inverse of the matrix.
+## If the matrix is already present, it instead returns
+## from the cachedcopy that we stored.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cached_i <- x$getMatrixInverse()
+  if (!is.null(cached_i)) {
+    message("Retriving cached copy.")
+    return(cached_i)
+  }
+  print(x$get())
+  c <- x$get()
+  cached_i <- solve(c, ...)
+  x$setMatrixInverse(cached_i)
+  return(cached_i)
 }
