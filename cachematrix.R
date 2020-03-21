fbf27e418ab40759310a5f151284fc6a70e04deb
babuaravind <itsbabuaravind@gmail.com>
2020-03-21 18:04:51 +0530

    Fixed logic.

--- cachematrix.R
+++ cachematrix.R
@@ -8,20 +8,26 @@
 ## this function is used to store the matrix and it's inverse.
 ## We can also retrieve the stored matrix by using get and getMatrixInverse inner functions.
 makeCacheMatrix <- function(x = matrix()) {
+  #Free Variables
   cached_i <- NULL
+  
+  setMatrixInverse <- function(matrixInverse) {
+    cached_i <<- matrixInverse
+  } 
+  
+  getMatrixInverse <- function() {
+    return(cached_i)
+  }
+  
   set <- function(y) {
     x <<- y
     cached_i <<- NULL
   }
+  
   get <- function() {
     return(x)
   }
-  setMatrixInverse <- function(matrixInverse) {
-    cached_i <<- matrixInverse
-  } 
-  getMatrixInverse <- function() {
-    return(cached_i)
-  }
+  
   list(set = set,
        get = get,
        setMatrixInverse = setMatrixInverse,
